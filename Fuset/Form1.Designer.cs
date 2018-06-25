ffe0612a00245c0f2e8c1539b5e69cf5f4f30611
Егор <blooddecember@gmail.com>
2018-06-25 15:34:15 +0200

    Разгадывает простые капчи

--- Fuset/Form1.Designer.cs
+++ Fuset/Form1.Designer.cs
@@ -31,6 +31,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -62,17 +72,93 @@
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 119);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(120, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(32, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 239);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(309, 128);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +167,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Fuset/Form1.cs
+++ Fuset/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
@@ -16,9 +17,13 @@ namespace Fuset
 {
     public partial class Form1 : Form
     {
+        string PuthToPicture; //Переменная для хранения пути к картинке капчи на компьютере
+        double winnings = 0;
+
         public Form1()
         {
             InitializeComponent();
+            Rucaptcha.Key = "";
         }
 
         private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
@@ -47,7 +52,7 @@ namespace Fuset
                         driver.FindElement(By.CssSelector(".login_menu_button")).Click();
                         richTextBox1.AppendText("Вход...");
                         driver.FindElement(By.Id("login_form_btc_address")).SendKeys("blooddecember@gmail.com");
-                        driver.FindElement(By.Id("login_form_password")).SendKeys("Problem.net87");
+                        driver.FindElement(By.Id("login_form_password")).SendKeys("");
                         driver.FindElement(By.Id("login_button")).Click();
                     }
                     catch (OpenQA.Selenium.NoSuchElementException)
@@ -58,6 +63,99 @@ namespace Fuset
                     //richTextBox1.AppendText(Convert.ToString((driver.FindElement(By.Id("btc_usd_price")).Text + "\n")));
                     break;
             }
+            driver.FindElement(By.Id("switch_captchas_button")).Click();
+            IWebElement logo = driver.FindElement(By.XPath("//*[@id='captchasnet_free_play_captcha']/div[1]/img"));
+            String logoSRC = logo.GetAttribute("src");
+            Uri imageURL = new Uri(logoSRC);
+            PuthToPicture = Rucaptcha.Download_Captcha(imageURL.ToString());
+            //Thread.Sleep(1000);
+            pictureBox1.ImageLocation = PuthToPicture;
+            try
+            {
+                driver.FindElement(By.XPath("//*[@id='captchasnet_free_play_captcha']/input[2]")).SendKeys(Rucaptcha.Recognize(PuthToPicture));
+            }
+            catch (OpenQA.Selenium.ElementNotVisibleException)
+            {
+                driver.FindElement(By.Id("switch_captchas_button")).Click();
+                driver.FindElement(By.XPath("//*[@id='captchasnet_free_play_captcha']/input[2]")).SendKeys(Rucaptcha.Recognize(PuthToPicture));
+            }
+            logo = driver.FindElement(By.XPath("//*[@id='botdetect_free_play_captcha']/div[1]/img"));
+            logoSRC = logo.GetAttribute("src");
+            imageURL = new Uri(logoSRC);
+            PuthToPicture = Rucaptcha.Download_Captcha(imageURL.ToString());
+            //Thread.Sleep(1000);
+            pictureBox2.ImageLocation = PuthToPicture;
+            try
+            {
+                driver.FindElement(By.XPath("//*[@id='botdetect_free_play_captcha']/input[2]")).SendKeys(Rucaptcha.Recognize(PuthToPicture));
+            }
+            catch (OpenQA.Selenium.ElementNotVisibleException)
+            {
+                driver.FindElement(By.Id("switch_captchas_button")).Click();
+                driver.FindElement(By.XPath("//*[@id='botdetect_free_play_captcha']/input[2]")).SendKeys(Rucaptcha.Recognize(PuthToPicture));
+            }
+
+            driver.FindElement(By.CssSelector(".close-reveal-modal")).Click();
+            winnings += Convert.ToDouble(driver.FindElement(By.Id("winnings")).Text);
+            label1.Text = Convert.ToString(winnings);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Записываем в настройки персональный Rucaptcha key
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+             * Скачиваем картинку по ссылке
+             * и сохраняем ее в папку \captcha_images\
+             * которая появится возле исполняемого файла (.exe)
+             * и записываем в переменную PuthToPicture 
+             * путь к картинке (капче), которую скачали
+             */
+            PuthToPicture = Rucaptcha.Download_Captcha("https://captchas.freebitco.in/cgi-bin/captcha_generator?client=freebitcoin&random=OsGPzk6irqVe4YX3xMxw8ivvtrYVsGC8");
+            /*
+             * Если переменная PuthToPicture пустая
+             * значит скачивание не произошло из-за
+             * отсутствия интернет-соединения
+             * либо из-за битой ссылки (URL)
+             * 
+             * Если же переменная не пустая - отображаем 
+             * в pictureBox1 скачавшуюся капчу
+             */
+            if (PuthToPicture != "")
+                pictureBox1.ImageLocation = PuthToPicture;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*
+             * Отправляем скачавшуюся ранее картинку на разгадывание
+             * Если ответ будет содержать слово ERROR, значит возникла 
+             * определенная ошибка, которая будет описана в ответе после слова ERROR            
+             * 
+             * В ином же случае, если слова ERROR в ответе не будет, значит сервис 
+             * вернет текст отправленной капчи 
+             */
+            MessageBox.Show(Rucaptcha.Recognize(PuthToPicture));
+            /* Ответ от функци, вероятно, придётся ждать определенное время
+             * Во-первых, мы ждем наличия свободного работника 
+             * бывает что все заняты, бывает что все свободны — всегда по разному
+             * И во-вторых, ждем, пока работник разгадает капчу 
+             * 
+             * И да, при использовании этой функции в определённых программах  рекомендую ее вешать 
+             * на отдельный поток, чтобы при этом не зависала основная форма программы
+             */
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            PuthToPicture = openFileDialog1.FileName;
+            if (PuthToPicture != "")
+                pictureBox1.ImageLocation = PuthToPicture;
         }
     }
 }
